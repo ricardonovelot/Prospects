f3e72eb404055933e60038f431a1e91bfc1e0997
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-06-05 18:11:25 -0600

    Commit

---
+++ HotProscpects/Notes.swift
@@ -0,0 +1,8 @@
+//
+//  Notes.swift
+//  HotProscpects
+//
+//  Created by Ricardo on 04/06/24.
+//
+
+import Foundation
---
+++ ProscpectFormView.swift
@@ -0,0 +1,18 @@
+//
+//  ProscpectFormView.swift
+//  HotProscpects
+//
+//  Created by Ricardo on 05/06/24.
+//
+
+import SwiftUI
+
+struct ProscpectFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProscpectFormView()
+}
